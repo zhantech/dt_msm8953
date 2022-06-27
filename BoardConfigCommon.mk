808ae22839fddbec70bb322fb490d3d3d5c427e2
GhostMaster69-dev <rathore6375@gmail.com>
2022-06-27 08:12:09 +0530

    msm8953-common: Compile kernel with `make LLVM=1`
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,7 +49,7 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
