f68af6f754152f4fa279a965f229b0b5f0e42ef8
minaripenguin37 <alexfinhart@gmail.com>
2022-10-09 10:06:23 +0530

    msm8953-common: Allow usage of custom binary linker (LD)
     - Inline with latest Change in source
    
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,6 +49,7 @@ ifeq ($(TARGET_KERNEL_VERSION),4.9)
 BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
 TARGET_KERNEL_CLANG_COMPILE := true
 endif
+TARGET_KERNEL_OPTIONAL_LD := true
 TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # ANT
