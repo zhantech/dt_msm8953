417c7ed9184e3dd578fc04a26b71e646b2363066
zhantech <hifzhan41@gmail.com>
2022-10-25 02:22:47 +0000

    msm8953-common: Switch to GCamGOPrebuilt-V2

--- msm8953.mk
+++ msm8953.mk
@@ -143,6 +143,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
+    GCamGOPrebuilt-V2 \
     libmm-qcamera \
     libstdc++.vendor \
     Snap
