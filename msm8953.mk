f13042f0f4de6627beb949063dfbda2ec0881dd9
zhantech <hifzhan41@gmail.com>
2022-11-20 08:16:26 +0000

    Revert "msm8953-common: Switch to GCamGOPrebuilt-V2"
    This reverts commit c393c0f80782dc6407d0d0001b8eba6359188786.

--- msm8953.mk
+++ msm8953.mk
@@ -139,7 +139,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
-    GCamGOPrebuilt-V2 \
     libstdc++.vendor \
     Snap
 
