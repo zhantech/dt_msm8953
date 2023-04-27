599e2fc1d459574462a97c655e8246573aef0f61
hklknz <haikalluthfianinobaluqia598@gmail.com>
2023-04-27 14:33:19 +0000

    Revert "msm8953-common: Allow usage of custom binary linker (LD)"
    This reverts commit f68af6f754152f4fa279a965f229b0b5f0e42ef8.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,7 +58,6 @@ ifeq ($(TARGET_KERNEL_VERSION),4.9)
 BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
 TARGET_KERNEL_CLANG_COMPILE := true
 endif
-TARGET_KERNEL_OPTIONAL_LD := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 LLVM_IAS=1 HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # ANT
